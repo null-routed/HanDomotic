e6fb16bd58f8640176a06bc60571ca8db92409f4
Lorenzo Ceccanti <ceccantilorenzo@gmail.com>
2024-05-14 18:10:11 +0200

    Java app now expects a tensor on 64 bits

--- classifierTest/app/src/main/java/it/unipi/masss/classifiertest/presentation/MainActivity.kt
+++ classifierTest/app/src/main/java/it/unipi/masss/classifiertest/presentation/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import it.unipi.masss.classifiertest.R
 import it.unipi.masss.classifiertest.presentation.theme.ClassifierTestTheme
+import java.nio.DoubleBuffer
 import java.nio.FloatBuffer
 import kotlin.random.Random
 
@@ -34,7 +35,7 @@ class MainActivity : ComponentActivity() {
     fun convertPrimitiveFloatArray2DToFloatObjectArray2D(floatArray2D: Array<FloatArray>): Array<Array<Float>> {
         return Array(floatArray2D.size) { i ->
             Array(floatArray2D[i].size) { j ->
-                floatArray2D[i][j]
+                floatArray2D[i][j].toFloat()
             }
         }
     }
@@ -45,22 +46,22 @@ class MainActivity : ComponentActivity() {
     }
 
     // Make predictions from a given input
-    private fun runPrediction(input: FloatArray, ortSession: OrtSession, ortEnvironment: OrtEnvironment): Pair<Array<Array<Float>>, Array<String>> {
+    private fun runPrediction(input: DoubleArray, ortSession: OrtSession, ortEnvironment: OrtEnvironment): Pair<Array<DoubleArray>, Array<String>> {
         // Get the name of the input node
         val inputName = ortSession.inputNames?.iterator()?.next()
         // Make a FloatBuffer of the inputs
-        val floatBufferInputs = FloatBuffer.wrap(input)
+        val doubleBufferInputs = DoubleBuffer.wrap(input)
         // Create input tensor with floatBufferInputs of shape (1, 30)
-        val inputTensor = OnnxTensor.createTensor(ortEnvironment, floatBufferInputs, longArrayOf(1, 30))
+        val inputTensor = OnnxTensor.createTensor(ortEnvironment, doubleBufferInputs, longArrayOf(1, 30))
         // Run the model
         val results = ortSession.run(mapOf(inputName to inputTensor))
-        val preProbabilities = results[1].value as Array<FloatArray>
-        val probabilities =  convertPrimitiveFloatArray2DToFloatObjectArray2D(preProbabilities) as Array<Array<Float>>
+        val preProbabilities = results[1].value as Array<DoubleArray>
+        // val probabilities =  convertPrimitiveFloatArray2DToFloatObjectArray2D(preProbabilities) as FloatArray
         val predictions = results[0].value as Array<String>
         inputTensor.close()
         // val output = results as Array<String>
         // Fetch and return the results as a string
-        return probabilities to predictions
+        return preProbabilities to predictions
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,13 +78,13 @@ class MainActivity : ComponentActivity() {
             // val inputs = FloatArray(floatArraySize) { Random.nextFloat() }
 
             // Trying with features related to a CIRCLE gesture (custom passed)
-            val inputs = floatArrayOf(
-                0.6714647492727273f, 1.3284390079341724f, -2.5785553f, 2.7940333f, 1.2450284143636359f,
-                121.8588309024358f, 6f, -0.4037437158434334f, 144.03753662356294f, 0.0f, -4.922348016181818f,
-                6.558244526993268f, -19.302053f, 7.66384f, 5.865837622000001f, 3698.2044697535434f, 5f,
-                -0.5550304390739136f, 4940.991313633613f, 0.0f, 4.861883669999999f, 8.529836947341735f,
-                -12.258312f, 17.104177f, 8.775228053636363f, 5301.781714286688f, 2f, -0.7441296674056268f,
-                6483.976438716447f, 0.01818181818181818f
+            val inputs = doubleArrayOf(
+                0.6714647492727273, 1.3284390079341724, -2.5785553, 2.7940333, 1.2450284143636359,
+                121.8588309024358, 6.0, -0.4037437158434334, 144.03753662356294, 0.0, -4.922348016181818,
+                6.558244526993268, -19.302053, 7.66384, 5.865837622000001, 3698.2044697535434, 5.0,
+                -0.5550304390739136, 4940.991313633613, 0.0, 4.861883669999999, 8.529836947341735,
+                -12.258312, 17.104177, 8.775228053636363, 5301.781714286688, 2.0, -0.7441296674056268,
+                6483.976438716447, 0.01818181818181818
             )
             println("FloatArray: ${inputs.joinToString()}")
 
