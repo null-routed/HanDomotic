59f3c385706dfdf32e08d11f2dfb3468b1d60fa7
Niccolò <niccolo.mule@gmail.com>
2024-05-22 12:00:31 +0200

    misc

--- handomotic/smartphoneApp/build.gradle.kts
+++ handomotic/smartphoneApp/build.gradle.kts
@@ -42,6 +42,7 @@ android {
     buildFeatures {
         compose = true
         dataBinding = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -67,6 +68,8 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.material)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
